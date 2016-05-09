b8a7fed3037b24063380696e936b30f9bec9e4ff
Pascal Honegger <pascal.honegger.external@atos.net>
2016-05-09 21:57:15 +0200

    Added MainMenu with some buttons

---
+++ Assets/MainMenu/Scripts/MainMenu.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+	public Texture BackgroundTexture;
+
+	public float RelativeDistanceLeft;
+	public float RelativeButtonWidth;
+	public float RelativeButtonHeight;
+
+	private float DistanceLeft
+	{
+		get { return Screen.width * RelativeDistanceLeft; }
+	}
+
+	private float ButtonWidth
+	{
+		get { return Screen.width * RelativeButtonWidth; }
+	}
+
+	private float ButtonHeight
+	{
+		get { return Screen.height* RelativeButtonHeight; }
+	}
+
+	void OnGUI() {
+		// Display Background Texture
+		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), BackgroundTexture);
+
+		//Display Buttons
+		if(GUI.Button(new Rect(DistanceLeft, Screen.height*.3f, ButtonWidth, ButtonHeight), "Play"))
+		{
+			print("Start pressed!");
+		}
+		if (GUI.Button(new Rect(DistanceLeft, Screen.height * .5f, ButtonWidth, ButtonHeight), "Options"))
+		{
+			print("Options pressed!");
+		}
+		if (GUI.Button(new Rect(DistanceLeft, Screen.height * .7f, ButtonWidth, ButtonHeight), "Highscore"))
+		{
+			print("Highscore pressed!");
+		}
+	}
+}
